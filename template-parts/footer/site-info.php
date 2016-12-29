b45eef0c0af98c5bff586bc0957f71ba6efc4253
Alana Martin <alana@stonybrookweb.com>
2016-12-29 17:21:50 -0500

    refator: update footer text

---
+++ template-parts/footer/site-info.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Displays footer site info
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+<div class="site-info">
+	<a href="<?php echo esc_url( __( 'http://www.stonybrookweb.com/', 'jpmap' ) ); ?>"><?php printf( __( 'Site by %s', 'jmap' ), 'Stony Brook Web' ); ?></a>
+</div><!-- .site-info -->
